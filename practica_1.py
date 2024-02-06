d2db16041eb878f776e15a6251b0a289464022f7
Derian Molina Lopez <derianmolina220@gmail.com>
2024-02-06 12:48:43 -0700

    mejorando el flujo

--- practica_1.py
+++ practica_1.py
@@ -5,8 +5,9 @@ categoria = 0
 def cargar_archivo():
     global lista_archivo
     with open("dataset.txt", 'r') as archivo:
-        lista_archivo = archivo.readlines()
-        lista_archivo = [linea.strip() + '\n' for linea in lista_archivo]
+        lista_archivo = archivo.readlines()        
+        lista_archivo = [linea.strip().lower() + '\n' for linea in lista_archivo]#agregando el lower al strip ya convierte en minuscula
+        print(lista_archivo)
 
 # opcion 1: ordenar por nombre
 def orden_nombre():
@@ -14,13 +15,17 @@ def orden_nombre():
     global categoria
 
     # ordenar por nombre
-    lista_archivo = sorted(lista_archivo, key=lambda x: x.split(', ')[0])
+    lista_archivo = sorted(lista_archivo, key=lambda x: x.split(', ')[0].lower())#aqui con el lower te ignora si hay una entrada en mayusucla
 
     print(lista_archivo)
 
     print()
     categoria = 1
 
+#este es un poco opcional, solo lo use para testear si se ordena correctamente
+def mostrarFormato ():
+    for linea in lista_archivo:
+      print(linea)
 # opcion 2: ordenar por peso
 def orden_peso():
     global lista_archivo
@@ -51,92 +56,32 @@ def orden_estatura():
 def insertar():
     global lista_archivo
     global categoria
-
+   
     nuevo_nombre = input("Ingresa nombre completo: ")
     nuevo_peso = input("Ingresa el peso de " + nuevo_nombre + ": ")
     nueva_estatura = input("Ingresa la estatura de " + nuevo_nombre + ": ")
 
     print()
-
     # guarda un nuevo elemento para el txt
     nuevo_registro = nuevo_nombre + ", " + nuevo_peso + ", " + nueva_estatura + "\n"
 
-    # NOMBRE
-    if categoria == 1:
-        # si es el menor, lo pone hasta arriba
-        if nuevo_nombre < lista_archivo[0].split(', ')[0]:
-            lista_archivo.insert(0, nuevo_registro)
-
-        # si es el mayor, lo pone hasta abajo
-        elif nuevo_nombre > lista_archivo[-1].split(', ')[0]:
-            lista_archivo.append(nuevo_registro)
-    
-        # busqueda lineal para encontrar en donde debe de ir
-        else:
-            for i in range(len(lista_archivo) - 1):
-                nombre_actual = lista_archivo[i].split(', ')[0]
-                nombre_siguiente = lista_archivo[i + 1].split(', ')[0]
-                if nombre_actual < nuevo_nombre < nombre_siguiente:
-                    lista_archivo.insert(i + 1, nuevo_registro)
-                    break
-
-        print(lista_archivo)
-
-        print()
-    
-    # PESO
-    if categoria == 2:
-        # si es el menor, lo pone hasta arriba
-        if int(nuevo_peso) < int(lista_archivo[0].split(', ')[1]):
-            lista_archivo.insert(0, nuevo_registro)
-        # si es el mayor, lo pone hasta abajo
-        elif int(nuevo_peso) > int(lista_archivo[-1].split(', ')[1]):
-            lista_archivo.append(nuevo_registro)
-    
-        # busqueda lineal para encontrar en donde debe de ir
-        else:
-            for i in range(len(lista_archivo) - 1):
-                peso_actual = int(lista_archivo[i].split(', ')[1])
-                peso_siguiente = int(lista_archivo[i + 1].split(', ')[1])
-                if peso_actual < int(nuevo_peso) < peso_siguiente:
-                    lista_archivo.insert(i + 1, nuevo_registro)
-                    break
-
-        print(lista_archivo)
-
-        print()
-
-    # ESTATURA
-    if categoria == 3:
-        # si es el menor, lo pone hasta arriba
-        if float(nueva_estatura) < float(lista_archivo[0].split(', ')[2]):
-            lista_archivo.insert(0, nuevo_registro)
-        # si es el mayor, lo pone hasta abajo
-        elif float(nueva_estatura) > float(lista_archivo[-1].split(', ')[2]):
-            lista_archivo.append(nuevo_registro)
-    
-        # busqueda lineal para encontrar en donde debe de ir
-        else:
-            for i in range(len(lista_archivo) - 1):
-                estatura_actual = float(lista_archivo[i].split(', ')[2])
-                estatura_siguiente = float(lista_archivo[i + 1].split(', ')[2])
-                if estatura_actual < float(nueva_estatura) < estatura_siguiente:
-                    lista_archivo.insert(i + 1, nuevo_registro)
-                    break
-
-        print(lista_archivo)
-
-        print()
+    #ordenamos por medio de la categoria que este activa
+    #si hay una categoria agregamos y ordenamos, si no, lo mandamos al final
+    if categoria !=0:
+        lista_archivo.append(nuevo_registro)
+        categorias[categoria]()
+        
+    else:
+        lista_archivo.append(nuevo_registro)
+    print(lista_archivo)
 
 # opcion 5: eliminar dato
 def eliminar():
     global lista_archivo
-    
     registro_eliminar = input("Ingresa el nombre completo del registro a eliminar: ")
 
     # borra el elemento de la lista (haciendo un filtro) y lo guarda en una nueva variable
     lista_nueva = [linea for linea in lista_archivo if registro_eliminar.casefold() != linea.split(', ')[0].casefold()]
-    
     lista_archivo = lista_nueva
     
     print()
@@ -146,7 +91,14 @@ def eliminar():
 
 # carga los datos antes de entrar al menu
 cargar_archivo()
-
+#con este diccionario podemos agilizar el proceso de seleccion con unas lineas
+categorias  = {
+    1: orden_nombre,
+    2: orden_peso,
+    3: orden_estatura,
+    4: insertar,
+    5: eliminar
+}
 while True:
     print("Practica 1:")
     print("1. Orden por nombre")
@@ -155,10 +107,14 @@ while True:
     print("4. Insertar un registro")
     print("5. Eliminar un registro")
     print("6. Salir")
-
-    opcion = input("Ingresa el numero para la opcion que quiera realizar: ")
-    print()
-
+    opcion = input("Elige una opcion: ")
+    #validar los rangos
+    if opcion >= '1' and opcion <= '6':
+        categorias[int(opcion)]()
+    else:
+        print("dato no valido!")
+        print()
+    '''
     if opcion == '1':
         orden_nombre()
     elif opcion == '2':
@@ -174,3 +130,5 @@ while True:
     else:
         print("dato no valido!")
         print()
+    '''
+    
